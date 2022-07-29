7f3557248080f7be4392cbffa0692102107842c5
sujalsh <sujalsharma350@gmail.com>
2022-07-29 13:24:11 +0530

    Create Stack_2_Duplicatebrackets.java

---
+++ javaProgramming/src/Stack_2_Duplicatebrackets.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Stack_2_Duplicatebrackets {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
+        Stack<Character> stack = new Stack();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if(ch == ')'){
+                if(stack.peek()=='('){
+                    System.out.println(true);
+                }
+                else{
+                    while(stack.peek()!='('){
+                        stack.pop();
+                    }
+                    stack.pop();
+                }
+            }
+            else{
+                stack.push(ch);
+            }
+        }
+        System.out.println(false);
+    }
+}
