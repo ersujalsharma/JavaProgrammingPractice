9b0c86d4f3b3f8ece73ea7710fda24c884987875
sujalsh <sujalsharma350@gmail.com>
2022-08-03 14:07:24 +0530

    hvgjhvkv

---
+++ javaProgramming/src/StringBuilder_Exaple_2.java
@@ -0,0 +1,22 @@
+public class StringBuilder_Exaple_2 {
+    public static void main(String[] args) {
+        String str = "";
+        int n = 10000;
+        double start_time = System.currentTimeMillis();
+
+        for (int i = 0; i < n; i++) {
+            str+=i;
+        }
+        double end_time = System.currentTimeMillis();
+        System.out.println(end_time-start_time);
+
+        StringBuilder stringBuilder = new StringBuilder();
+        start_time = System.currentTimeMillis();
+        n=100000;
+        for (int i = 0; i < n; i++) {
+            stringBuilder.append(i);
+        }
+        end_time = System.currentTimeMillis();
+        System.out.println(end_time-start_time);
+    }
+}
